2d24386618fd5d83c66c5cc576fe146c51bb2fe0
Scott Sullivan <scott@xrdev.com>
2021-11-24 03:29:22 -0600

    add store and cryptoApi with redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+}
+
+const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
